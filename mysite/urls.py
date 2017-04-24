ae180875ceded1dd2bbcd48c41681cc6f6315f10
InPursuitPL <inpursuitpl@gmail.com>
2017-04-24 12:02:52 +0200

    Django yourFinance_2_0, initial commit, version with different models

---
+++ mysite/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.contrib.auth import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('yourFinance.urls')),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout',
+        kwargs={'next_page': '/'}),
+]
---
+++ yourFinance/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+from .forms import RegistrationForm
+
+
+def index(request):
+    return render(request, 'yourFinance/index.html')
+
+def register_page(request):
+    """Page for new user registration."""
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(username=form.cleaned_data['username'],
+                                        password=form.cleaned_data['password1'],
+                                        email=form.cleaned_data['email'])
+            return render(request, 'registration/register_success.html',
+                          {'user': form.cleaned_data['username']})
+        else:
+            return render(request, 'registration/register.html', {'form': form})
+    form = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
